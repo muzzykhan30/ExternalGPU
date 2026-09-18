b6d601f9bbf670e3fb4d92af8e352e7949cb2481
Mustafa Maqsood <66792421+muzzykhan30@users.noreply.github.com>
2026-09-18 16:06:42 +0530

    Add files via upload
    This .bat file will automatically detect and install your new GPU driver for you.

---
+++ detect_gpu.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+title Windows GPU Auto-Detector and Driver Portal Launcher
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0detect_gpu.ps1" %*
+if errorlevel 1 (
+    echo.
+    echo Script encountered an error.
+    pause
+)
